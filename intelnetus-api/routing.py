36cad292eedfd2c7e0b1b73b71b73fbf1b2a04c7
Gemyl <115283416+Gemyl@users.noreply.github.com>
2024-12-28 17:53:12 +0200

    feat(Metadata Insights): :sparkles: add API for retrieving publications number per country
    retrieve the number of publications for each one of the top 5 countries for a given period of time

--- intelnetus-api/routing.py
+++ intelnetus-api/routing.py
@@ -1,8 +1,14 @@
 from routes.GetMetadata import get_metadata
+from routes.GetInsights import get_publications_number_per_country
 
 def register_routes(app, db, is_production_env, scopus_api_key):
 
     @app.route('/get-metadata', methods=['GET'])
     def invoke_get_metadata():
         result = get_metadata(db, is_production_env, scopus_api_key)
+        return result
+    
+    @app.route('/get-insights/publications-number-per-country', methods=['GET'])
+    def invoke_get_insights():
+        result = get_publications_number_per_country(db, is_production_env)
         return result
