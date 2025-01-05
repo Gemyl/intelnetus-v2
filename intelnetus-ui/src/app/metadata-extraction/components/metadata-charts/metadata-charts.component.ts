e92eef3df6f48c5ca1fbad16f5830722574a2a6f
Gemyl <115283416+Gemyl@users.noreply.github.com>
2025-01-05 13:33:54 +0200

    style(Metadata Insights): :lipstick: display charts in case of no data are returned
    display charts in Metadata Insights dashboard in case no data are returned

--- intelnetus-ui/src/app/metadata-extraction/components/metadata-charts/metadata-charts.component.ts
+++ intelnetus-ui/src/app/metadata-extraction/components/metadata-charts/metadata-charts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.state';
@@ -8,7 +8,7 @@ import { AppState } from 'src/app/app.state';
   templateUrl: './metadata-charts.component.html',
   styleUrl: './metadata-charts.component.scss'
 })
-export class MetadataChartsComponent implements OnInit {
+export class MetadataChartsComponent implements OnInit, AfterViewInit {
   public publicationsPerCountryChart: Chart;
   public publicationsCitationsPerFieldChart: Chart;
   public fieldsCitationsPerYearChart: Chart;
@@ -22,63 +22,73 @@ export class MetadataChartsComponent implements OnInit {
       .select((store) => store.metadataInsightsState)
       .subscribe((metadataInsightsState) => {
         // publications number per country
-        let countries: Array<string> = [];
-        let publicationsNumberPerCountry: Array<number> = [];
-
-        if(Object.keys(metadataInsightsState.publicationsNumberPerCountry).length > 0) {
-          countries = Object.keys(metadataInsightsState.publicationsNumberPerCountry);
-          publicationsNumberPerCountry = Object.values(metadataInsightsState.publicationsNumberPerCountry);
+        if(this.publicationsPerCountryChart) {
+          let countries: Array<string> = ["No Data"];
+          let publicationsNumberPerCountry: Array<number> = [1];
+          
+          if(Object.keys(metadataInsightsState.publicationsNumberPerCountry).length > 0) {
+            countries = Object.keys(metadataInsightsState.publicationsNumberPerCountry);
+            publicationsNumberPerCountry = Object.values(metadataInsightsState.publicationsNumberPerCountry);
+          }
+          
+          this.publicationsPerCountryChart.data.labels = countries;
+          this.publicationsPerCountryChart.data.datasets = [{
+            data: publicationsNumberPerCountry
+          }];
+          
+          this.publicationsPerCountryChart.update();
         }
-
-        this.publicationsPerCountryChart.data.labels = countries;
-        this.publicationsPerCountryChart.data.datasets = [{
-          data: publicationsNumberPerCountry
-        }];
-
-        this.publicationsPerCountryChart.update();
 
         // publications number per field
-        let fields: Array<string> = [];
-        let citationsNumberPerField: Array<{label: string, data: Array<number>}> = [];
-
-        if(Object.keys(metadataInsightsState.publicationsCitationsNumberPerField).length > 0) {
-          fields = Object.keys(metadataInsightsState.publicationsCitationsNumberPerField);
-          citationsNumberPerField = [{label: "Citations", data: Object.values(metadataInsightsState.publicationsCitationsNumberPerField)}];
+        if(this.publicationsCitationsPerFieldChart) {
+          let fields: Array<string> = [];
+          let citationsNumberPerField: Array<{label: string, data: Array<number>}> = [{label:"No Data", data:[0]}];
+  
+          if(Object.keys(metadataInsightsState.publicationsCitationsNumberPerField).length > 0) {
+            fields = Object.keys(metadataInsightsState.publicationsCitationsNumberPerField);
+            citationsNumberPerField = [{label: "Citations", data: Object.values(metadataInsightsState.publicationsCitationsNumberPerField)}];
+          }
+  
+          this.publicationsCitationsPerFieldChart.data.labels = fields;
+          this.publicationsCitationsPerFieldChart.data.datasets = citationsNumberPerField;
+  
+          this.publicationsCitationsPerFieldChart.update();
         }
-
-        this.publicationsCitationsPerFieldChart.data.labels = fields;
-        this.publicationsCitationsPerFieldChart.data.datasets = citationsNumberPerField;
-
-        this.publicationsCitationsPerFieldChart.update();
 
         // fields citations per year
-        let years: Array<string> = [];
-        let fieldsCitationsPerYear: Array<{label: string, data: Array<number>}> = [];
-
-        if(Object.keys(metadataInsightsState.fieldsCitationsPerYear).length > 0) {
-          Object.keys(metadataInsightsState.fieldsCitationsPerYear).forEach((field) => {
-            if(Object.keys(metadataInsightsState.fieldsCitationsPerYear).indexOf(field) == 0) {
-              years = Object.keys(metadataInsightsState.fieldsCitationsPerYear[field]);
-            }
+        if(this.fieldsCitationsPerYearChart) {
+          let years: Array<string> = [];
+          let fieldsCitationsPerYear: Array<{label: string, data: Array<number>}> = [{label:"No Data", data:[0]}];
   
-            fieldsCitationsPerYear.push({
-              label: field,
-              data: Object.values(metadataInsightsState.fieldsCitationsPerYear[field])
-            })
-          });
+          if(Object.keys(metadataInsightsState.fieldsCitationsPerYear).length > 0) {
+            Object.keys(metadataInsightsState.fieldsCitationsPerYear).forEach((field) => {
+              if(Object.keys(metadataInsightsState.fieldsCitationsPerYear).indexOf(field) == 0) {
+                years = Object.keys(metadataInsightsState.fieldsCitationsPerYear[field]);
+              }
+    
+              fieldsCitationsPerYear.push({
+                label: field,
+                data: Object.values(metadataInsightsState.fieldsCitationsPerYear[field])
+              })
+            });
+          }
+  
+          this.fieldsCitationsPerYearChart.data.labels = years;
+          this.fieldsCitationsPerYearChart.data.datasets = fieldsCitationsPerYear;
+  
+          this.fieldsCitationsPerYearChart.update(); 
         }
-
-        this.fieldsCitationsPerYearChart.data.labels = years;
-        this.fieldsCitationsPerYearChart.data.datasets = fieldsCitationsPerYear;
-
-        this.fieldsCitationsPerYearChart.update(); 
       })
+  }
 
+  ngAfterViewInit(): void {
     this.publicationsPerCountryChart = new Chart("publications-per-country-chart", {
       type: "doughnut",
       data: {
-        labels: [],
-        datasets: []
+        labels: ["No Data"],
+        datasets: [{
+          data: [1]
+        }]
       },
       options: {
         plugins: {
@@ -102,7 +112,10 @@ export class MetadataChartsComponent implements OnInit {
       type: "bar",
       data: {
         labels: [],
-        datasets: []
+        datasets: [{
+          label: "No Data",
+          data: [0]
+        }]
       },
       options: {
         plugins: {
@@ -125,7 +138,10 @@ export class MetadataChartsComponent implements OnInit {
       type: "line",
       data: {
         labels: [],
-        datasets: []
+        datasets: [{
+          label: "No Data",
+          data: [0]
+        }]
       },
       options: {
         plugins: {
